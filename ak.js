65c3af2557b78e93598f4ec578b7399dea92c46b
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-03 23:08:27 +0530

    ghj

--- ak.js
+++ ak.js
@@ -1,6 +1,11 @@
 
-let a ,  b = 10;
+// let a=10 ,  b = 10;
 
 
-console.log(a+b);
-console.log(typeof a);
+// console.log(a+b);
+// console.log(typeof undefined);
+
+
+let k=10;
+let m=20;
+console.log(typeof (k,m));
