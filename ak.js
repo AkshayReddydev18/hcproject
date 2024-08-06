fb26712911b8301ffae8a7a1d03d01141d34ae04
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-06 23:31:31 +0530

    bhvcs

--- ak.js
+++ ak.js
@@ -6,6 +6,28 @@
 // console.log(typeof undefined);
 
 
-let k=10;
-let m=20;
-console.log(typeof (k,m));
+// let k=10;
+// let m='hello';
+// console.log(typeof (m,k));
+
+
+//  let obj={
+//     city:"hyd",
+//     country:"india",
+//     gender:"male",
+//     skill:["html","css","js",["sus","node.js","react","exp.js"]],
+//     address:{
+//       road_no:"2",
+//       street:"kphp",
+//       city:"hyd"}
+//     }
+// console.log(`my address is ${obj.address.street} at ${obj.address.city}`)
+// console.log("my address is "+obj.address.street+" at "+ obj.address.city)
+
+
+
+ let x=+prompt("enter a number");
+ let y=+prompt("enter a number");
+ let z=x+y;
+ alert("answer is"+z);
+ 
