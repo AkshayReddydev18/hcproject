758c145b087305de419bf8fcfbcd5f80e2f791b3
AkshayReddydev18 <poddukuriakshayreddy1605@gmail.com>
2024-08-07 13:43:23 +0530

    bhs

---
+++ ja5.js
@@ -0,0 +1,23 @@
+// let onetimeview = Boolean(+prompt("0"));
+
+// if (onetimeview) {
+//   console.log(x);
+//   console.log("message cannot be forwarded");
+// } else {
+//   console.log(x);
+//   console.log("message can be forwarded");
+// }
+
+let a = +prompt("enter a number");
+let b = +prompt("enter a number");
+let c = +prompt("enter a number");
+let z = a + b + c;
+if (Z == 180) {
+  if (a == 90 || b == 90 || c == 90) {
+    alert("it is a  right angled triangle");
+  } else {
+    alert(" it is not a right angled triangle");
+  }
+} else {
+  alert(" it is not a right angled triangle");
+}
